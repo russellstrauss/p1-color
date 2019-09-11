cdf8a26f3669a65c337094aaaa65ab7042a7a5ac
Russell Strauss <russellstrauss@gmail.com>
2019-09-10 20:59:36 -0400

    Initial commit

---
+++ assets/js/bundle.js
@@ -0,0 +1,415 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+module.exports = function () {
+  var renderer, scene, camera, controls;
+  var stats = new Stats();
+  var wireframeMaterial = new THREE.MeshBasicMaterial({
+    wireframe: true,
+    color: 0x08CDFA
+  });
+  var shadeMaterial = new THREE.MeshPhongMaterial({
+    color: 0x08CDFA,
+    side: THREE.DoubleSide,
+    opacity: .5,
+    transparent: true
+  });
+  var distinctColors = [new THREE.Color('#e6194b'), new THREE.Color('#3cb44b'), new THREE.Color('#ffe119'), new THREE.Color('#4363d8'), new THREE.Color('#f58231'), new THREE.Color('#911eb4'), new THREE.Color('#46f0f0'), new THREE.Color('#f032e6'), new THREE.Color('#bcf60c'), new THREE.Color('#fabebe'), new THREE.Color('#008080'), new THREE.Color('#e6beff'), new THREE.Color('#9a6324'), new THREE.Color('#fffac8'), new THREE.Color('#800000'), new THREE.Color('#aaffc3'), new THREE.Color('#808000'), new THREE.Color('#ffd8b1'), new THREE.Color('#000075'), new THREE.Color('#808080'), new THREE.Color('#ffffff'), new THREE.Color('#000000')];
+  return {
+    settings: {
+      activateLightHelpers: false,
+      axesHelper: {
+        activateAxesHelper: false,
+        axisLength: 10
+      },
+      activateStatsFPS: false,
+      font: {
+        enable: true,
+        fontStyle: {
+          font: null,
+          size: 1,
+          height: 0,
+          curveSegments: 1
+        }
+      },
+      messageDuration: 2000
+    },
+    init: function init() {
+      var self = this;
+      self.loadFont();
+      self.setUpButtons();
+    },
+    begin: function begin() {
+      var self = this;
+      self.setUpScene();
+      self.addFloor();
+      self.enableControls();
+      self.resizeRendererOnWindowResize();
+      self.setUpLights();
+      self.addGeometries();
+      camera.position.x = -20;
+      camera.position.y = 20;
+      camera.position.z = 20;
+
+      if (self.settings.activateStatsFPS) {
+        self.enableStats();
+      }
+
+      var animate = function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        controls.update();
+        stats.update();
+      };
+
+      animate();
+    },
+    addGeometries: function addGeometries() {
+      var self = this;
+      var radius = 5,
+          torusRadius = 3,
+          radialSegments = 16,
+          tubularSegments = 25;
+      var geometry = new THREE.TorusGeometry(radius, torusRadius, radialSegments, tubularSegments);
+      geometry.translate(0, radius + torusRadius, 0);
+      var material = new THREE.MeshBasicMaterial(wireframeMaterial);
+      var torus = new THREE.Mesh(geometry, material);
+      scene.add(torus);
+    },
+    enableControls: function enableControls() {
+      controls = new THREE.OrbitControls(camera, renderer.domElement);
+      controls.target.set(0, 0, 0);
+      controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+
+      controls.dampingFactor = 0.05;
+      controls.zoomSpeed = 6;
+      controls.enablePan = !utils.mobile();
+      controls.minDistance = 18;
+      controls.maxDistance = 100;
+      controls.maxPolarAngle = Math.PI / 2;
+    },
+    enableStats: function enableStats() {
+      document.body.appendChild(stats.dom);
+    },
+    setUpLights: function setUpLights() {
+      var self = this;
+      var lights = [];
+      var color = 0xFFFFFF;
+      var intensity = 1;
+      var light = new THREE.DirectionalLight(color, intensity);
+      light.position.set(-1, 2, 4);
+      scene.add(light);
+      lights.push(light);
+      var light2 = new THREE.DirectionalLight(color, intensity);
+      light2.position.set(0, 2, -8);
+      scene.add(light2);
+      lights.push(light2);
+
+      if (self.settings.activateLightHelpers) {
+        self.activateLightHelpers(lights);
+      }
+    },
+    activateLightHelpers: function activateLightHelpers(lights) {
+      for (var i = 0; i < lights.length; i++) {
+        var helper = new THREE.DirectionalLightHelper(lights[i], 5, 0x00000);
+        scene.add(helper);
+      }
+    },
+    addFloor: function addFloor() {
+      var planeGeometry = new THREE.PlaneBufferGeometry(100, 100);
+      planeGeometry.rotateX(-Math.PI / 2);
+      var planeMaterial = new THREE.ShadowMaterial({
+        opacity: 0.2
+      });
+      var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+      plane.position.y = -1;
+      plane.receiveShadow = true;
+      scene.add(plane);
+      var helper = new THREE.GridHelper(1000, 100);
+      helper.material.opacity = .25;
+      helper.material.transparent = true;
+      scene.add(helper);
+    },
+    setUpScene: function setUpScene() {
+      var self = this;
+      scene = new THREE.Scene();
+      scene.background = new THREE.Color(0xf0f0f0);
+      camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document.body.appendChild(renderer.domElement);
+
+      if (self.settings.axesHelper.activateAxesHelper) {
+        self.activateAxesHelper();
+      }
+    },
+    showPoints: function showPoints(geometry, color, opacity) {
+      var self = this;
+
+      for (var i = 0; i < geometry.vertices.length; i++) {
+        self.showPoint(geometry.vertices[i], color, opacity);
+      }
+    },
+    showPoint: function showPoint(pt, color, opacity) {
+      color = color || 0xff0000;
+      opacity = opacity || 1;
+      var dotGeometry = new THREE.Geometry();
+      dotGeometry.vertices.push(new THREE.Vector3(pt.x, pt.y, pt.z));
+      var dotMaterial = new THREE.PointsMaterial({
+        size: 10,
+        sizeAttenuation: false,
+        color: color,
+        opacity: opacity,
+        transparent: true
+      });
+      var dot = new THREE.Points(dotGeometry, dotMaterial);
+      scene.add(dot);
+    },
+    showVector: function showVector(vector, origin, color) {
+      color = color || 0xff0000;
+      var arrowHelper = new THREE.ArrowHelper(vector, origin, vector.length(), color);
+      scene.add(arrowHelper);
+    },
+    drawLine: function drawLine(pt1, pt2) {
+      var material = new THREE.LineBasicMaterial({
+        color: 0x0000ff
+      });
+      var geometry = new THREE.Geometry();
+      geometry.vertices.push(new THREE.Vector3(pt1.x, pt1.y, pt1.z));
+      geometry.vertices.push(new THREE.Vector3(pt2.x, pt2.y, pt2.z));
+      var line = new THREE.Line(geometry, material);
+      scene.add(line);
+    },
+    getDistance: function getDistance(pt1, pt2) {
+      // create point class?
+      var squirt = Math.pow(pt2.x - pt1.x, 2) + Math.pow(pt2.y - pt1.y, 2) + Math.pow(pt2.z - pt1.z, 2);
+      return Math.sqrt(squirt);
+    },
+    createVector: function createVector(pt1, pt2) {
+      return new THREE.Vector3(pt2.x - pt1.x, pt2.y - pt2.y, pt2.z - pt1.z);
+    },
+    getMidpoint: function getMidpoint(pt1, pt2) {
+      var midpoint = {};
+      midpoint.x = (pt1.x + pt2.x) / 2;
+      midpoint.y = (pt1.y + pt2.y) / 2;
+      midpoint.z = (pt1.z + pt2.z) / 2;
+      return midpoint;
+    },
+    createTriangle: function createTriangle(pt1, pt2, pt3) {
+      // return geometry
+      var triangleGeometry = new THREE.Geometry();
+      triangleGeometry.vertices.push(new THREE.Vector3(pt1.x, pt1.y, pt1.z));
+      triangleGeometry.vertices.push(new THREE.Vector3(pt2.x, pt2.y, pt2.z));
+      triangleGeometry.vertices.push(new THREE.Vector3(pt3.x, pt3.y, pt3.z));
+      triangleGeometry.faces.push(new THREE.Face3(0, 1, 2));
+      triangleGeometry.computeFaceNormals();
+      return triangleGeometry;
+    },
+    activateAxesHelper: function activateAxesHelper() {
+      var self = this;
+      var axesHelper = new THREE.AxesHelper(self.settings.axesHelper.axisLength);
+      scene.add(axesHelper);
+    },
+    labelAxes: function labelAxes() {
+      var self = this;
+
+      if (self.settings.font.enable) {
+        var textGeometry = new THREE.TextGeometry('Y', self.settings.font.fontStyle);
+        var textMaterial = new THREE.MeshBasicMaterial({
+          color: 0x00ff00
+        });
+        var mesh = new THREE.Mesh(textGeometry, textMaterial);
+        textGeometry.translate(0, self.settings.axesHelper.axisLength, 0);
+        scene.add(mesh);
+        textGeometry = new THREE.TextGeometry('X', self.settings.font.fontStyle);
+        textMaterial = new THREE.MeshBasicMaterial({
+          color: 0xff0000
+        });
+        mesh = new THREE.Mesh(textGeometry, textMaterial);
+        textGeometry.translate(self.settings.axesHelper.axisLength, 0, 0);
+        scene.add(mesh);
+        textGeometry = new THREE.TextGeometry('Z', self.settings.font.fontStyle);
+        textMaterial = new THREE.MeshBasicMaterial({
+          color: 0x0000ff
+        });
+        mesh = new THREE.Mesh(textGeometry, textMaterial);
+        textGeometry.translate(0, 0, self.settings.axesHelper.axisLength);
+        scene.add(mesh);
+      }
+    },
+    loadFont: function loadFont() {
+      var self = this;
+      var loader = new THREE.FontLoader();
+      var fontPath = '';
+      fontPath = 'assets/vendors/js/three.js/examples/fonts/helvetiker_regular.typeface.json';
+      loader.load(fontPath, function (font) {
+        // success event
+        console.log('Fonts loaded successfully.');
+        self.settings.font.fontStyle.font = font;
+        self.begin();
+        if (self.settings.axesHelper.activateAxesHelper) self.labelAxes();
+      }, function (event) {
+        // in progress event.
+        console.log('Attempting to load fonts.');
+      }, function (event) {
+        // error event
+        console.log('Error loading fonts. Webserver required due to CORS policy.');
+        self.settings.font.enable = false;
+        self.begin();
+      });
+    },
+
+    /* 	Inputs: pt - point in space to label, in the form of object with x, y, and z properties; label - text content for label; color - optional */
+    labelPoint: function labelPoint(pt, label, color) {
+      var self = this;
+
+      if (self.settings.font.enable) {
+        color = color || 0xff0000;
+        var textGeometry = new THREE.TextGeometry(label, self.settings.font.fontStyle);
+        var textMaterial = new THREE.MeshBasicMaterial({
+          color: color
+        });
+        var mesh = new THREE.Mesh(textGeometry, textMaterial);
+        textGeometry.translate(pt.x, pt.y, pt.z);
+        scene.add(mesh);
+      }
+    },
+    setUpButtons: function setUpButtons() {
+      var self = this;
+      var message = document.getElementById('message');
+      document.addEventListener('keyup', function (event) {
+        var L = 76;
+
+        if (event.keyCode === L) {// do stuff when pressing key
+        }
+      });
+    },
+    resizeRendererOnWindowResize: function resizeRendererOnWindowResize() {
+      window.addEventListener('resize', utils.debounce(function () {
+        if (renderer) {
+          camera.aspect = window.innerWidth / window.innerHeight;
+          camera.updateProjectionMatrix();
+          renderer.setSize(window.innerWidth, window.innerHeight);
+        }
+      }, 250));
+    }
+  };
+};
+
+},{}],2:[function(require,module,exports){
+"use strict";
+
+var Scene = require('./components/scene.js');
+
+var Utilities = require('./utils.js');
+
+(function () {
+  document.addEventListener("DOMContentLoaded", function () {
+    Scene().init();
+  });
+})();
+
+},{"./components/scene.js":1,"./utils.js":3}],3:[function(require,module,exports){
+"use strict";
+
+(function () {
+  var appSettings;
+
+  window.utils = function () {
+    return {
+      appSettings: {
+        breakpoints: {
+          mobileMax: 767,
+          tabletMin: 768,
+          tabletMax: 991,
+          desktopMin: 992,
+          desktopLargeMin: 1200
+        }
+      },
+      mobile: function mobile() {
+        return window.innerWidth < this.appSettings.breakpoints.tabletMin;
+      },
+      tablet: function tablet() {
+        return window.innerWidth > this.appSettings.breakpoints.mobileMax && window.innerWidth < this.appSettings.breakpoints.desktopMin;
+      },
+      desktop: function desktop() {
+        return window.innerWidth > this.appSettings.breakpoints.desktopMin;
+      },
+      getBreakpoint: function getBreakpoint() {
+        if (window.innerWidth < this.appSettings.breakpoints.tabletMin) return 'mobile';else if (window.innerWidth < this.appSettings.breakpoints.desktopMin) return 'tablet';else return 'desktop';
+      },
+      debounce: function debounce(func, wait, immediate) {
+        var timeout;
+        return function () {
+          var context = this,
+              args = arguments;
+
+          var later = function later() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+          };
+
+          var callNow = immediate && !timeout;
+          clearTimeout(timeout);
+          timeout = setTimeout(later, wait);
+          if (callNow) func.apply(context, args);
+        };
+      },
+
+      /* Purpose: Detect if any of the element is currently within the viewport */
+      anyOnScreen: function anyOnScreen(element) {
+        var win = $(window);
+        var viewport = {
+          top: win.scrollTop(),
+          left: win.scrollLeft()
+        };
+        viewport.right = viewport.left + win.width();
+        viewport.bottom = viewport.top + win.height();
+        var bounds = element.offset();
+        bounds.right = bounds.left + element.outerWidth();
+        bounds.bottom = bounds.top + element.outerHeight();
+        return !(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom);
+      },
+
+      /* Purpose: Detect if an element is vertically on screen; if the top and bottom of the element are both within the viewport. */
+      allOnScreen: function allOnScreen(element) {
+        var win = $(window);
+        var viewport = {
+          top: win.scrollTop(),
+          left: win.scrollLeft()
+        };
+        viewport.right = viewport.left + win.width();
+        viewport.bottom = viewport.top + win.height();
+        var bounds = element.offset();
+        bounds.right = bounds.left + element.outerWidth();
+        bounds.bottom = bounds.top + element.outerHeight();
+        return !(viewport.bottom < bounds.top && viewport.top > bounds.bottom);
+      },
+      secondsToMilliseconds: function secondsToMilliseconds(seconds) {
+        return seconds * 1000;
+      },
+
+      /*
+      * Purpose: This method allows you to temporarily disable an an element's transition so you can modify its proprties without having it animate those changing properties.
+      * Params:
+      * 	-element: The element you would like to modify.
+      * 	-cssTransformation: The css transformation you would like to make, i.e. {'width': 0, 'height': 0} or 'border', '1px solid black'
+      */
+      getTransitionDuration: function getTransitionDuration(element) {
+        var $element = $(element);
+        return utils.secondsToMilliseconds(parseFloat(getComputedStyle($element[0])['transitionDuration']));
+      },
+      isInteger: function isInteger(number) {
+        return number % 1 === 0;
+      },
+      rotate: function rotate(array) {
+        array.push(array.shift());
+        return array;
+      }
+    };
+  }();
+
+  module.exports = window.utils;
+})();
+
+},{}]},{},[2]);
---
+++ assets/js/components/scene.js
@@ -0,0 +1,334 @@
+module.exports = function() {
+	
+	var renderer, scene, camera, controls;
+	var stats = new Stats();
+	var wireframeMaterial = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x08CDFA });
+	var shadeMaterial = new THREE.MeshPhongMaterial({
+		color: 0x08CDFA,
+		side: THREE.DoubleSide,
+		opacity: .5,
+		transparent: true
+	});
+	var distinctColors = [new THREE.Color('#e6194b'), new THREE.Color('#3cb44b'), new THREE.Color('#ffe119'), new THREE.Color('#4363d8'), new THREE.Color('#f58231'), new THREE.Color('#911eb4'), new THREE.Color('#46f0f0'), new THREE.Color('#f032e6'), new THREE.Color('#bcf60c'), new THREE.Color('#fabebe'), new THREE.Color('#008080'), new THREE.Color('#e6beff'), new THREE.Color('#9a6324'), new THREE.Color('#fffac8'), new THREE.Color('#800000'), new THREE.Color('#aaffc3'), new THREE.Color('#808000'), new THREE.Color('#ffd8b1'), new THREE.Color('#000075'), new THREE.Color('#808080'), new THREE.Color('#ffffff'), new THREE.Color('#000000')];
+	
+	return {
+		
+		settings: {
+			activateLightHelpers: false,
+			axesHelper: {
+				activateAxesHelper: false,
+				axisLength: 10
+			},
+			activateStatsFPS: false,
+			font: {
+				enable: true,
+				fontStyle: {
+					font: null,
+					size: 1,
+					height: 0,
+					curveSegments: 1
+				}
+			},
+			messageDuration: 2000
+		},
+		
+		init: function() {
+
+			let self = this;
+			self.loadFont();
+			self.setUpButtons();
+		},
+		
+		begin: function() {
+			
+			let self = this;
+			
+			self.setUpScene();
+			self.addFloor();
+			self.enableControls();
+			self.resizeRendererOnWindowResize();
+			self.setUpLights();
+			self.addGeometries();
+			
+			camera.position.x = -20;
+			camera.position.y = 20;
+			camera.position.z = 20;
+			
+			if (self.settings.activateStatsFPS) {
+				self.enableStats();
+			}
+			
+			var animate = function() {
+
+				requestAnimationFrame(animate);
+				renderer.render(scene, camera);
+				controls.update();
+				stats.update();
+			};
+			
+			animate();
+		},
+		
+		addGeometries: function() {
+			
+			let self = this;
+			
+			let radius = 5, torusRadius = 3, radialSegments = 16, tubularSegments = 25;
+			
+			let geometry = new THREE.TorusGeometry(radius, torusRadius, radialSegments, tubularSegments);
+			geometry.translate(0, radius + torusRadius, 0);
+			let material = new THREE.MeshBasicMaterial(wireframeMaterial);
+			let torus = new THREE.Mesh(geometry, material);
+			scene.add(torus);
+		},
+
+		enableControls: function() {
+			controls = new THREE.OrbitControls(camera, renderer.domElement);
+			controls.target.set(0, 0, 0);
+			controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+			controls.dampingFactor = 0.05;
+			controls.zoomSpeed = 6;
+			controls.enablePan = !utils.mobile();
+			controls.minDistance = 18;
+			controls.maxDistance = 100;
+			controls.maxPolarAngle = Math.PI / 2;
+		},
+
+		enableStats: function() {
+			document.body.appendChild(stats.dom);
+		},
+
+		setUpLights: function() {
+
+			let self = this;
+			let lights = [];
+			const color = 0xFFFFFF;
+			const intensity = 1;
+			const light = new THREE.DirectionalLight(color, intensity);
+			light.position.set(-1, 2, 4);
+			scene.add(light);
+			lights.push(light);
+
+			const light2 = new THREE.DirectionalLight(color, intensity);
+			light2.position.set(0, 2, -8);
+			scene.add(light2);
+			lights.push(light2)
+			
+			if (self.settings.activateLightHelpers) {
+				self.activateLightHelpers(lights);
+			}
+		},
+
+		activateLightHelpers: function(lights) {
+
+			for (let i = 0; i < lights.length; i++) {
+				let helper = new THREE.DirectionalLightHelper(lights[i], 5, 0x00000);
+				scene.add(helper);
+			}
+		},
+
+		addFloor: function() {
+			var planeGeometry = new THREE.PlaneBufferGeometry(100, 100);
+			planeGeometry.rotateX(-Math.PI / 2);
+			var planeMaterial = new THREE.ShadowMaterial({ opacity: 0.2 });
+
+			var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+			plane.position.y = -1;
+			plane.receiveShadow = true;
+			scene.add(plane);
+
+			var helper = new THREE.GridHelper(1000, 100);
+			helper.material.opacity = .25;
+			helper.material.transparent = true;
+			scene.add(helper);
+		},
+
+		setUpScene: function() {
+
+			let self = this;
+			scene = new THREE.Scene();
+			scene.background = new THREE.Color(0xf0f0f0);
+			camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+			renderer = new THREE.WebGLRenderer();
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			document.body.appendChild(renderer.domElement);
+
+			if (self.settings.axesHelper.activateAxesHelper) {
+
+				self.activateAxesHelper();
+			}
+		},
+		
+		showPoints: function(geometry, color, opacity) {
+			
+			let self = this;
+			
+			for (let i = 0; i < geometry.vertices.length; i++) {
+				self.showPoint(geometry.vertices[i], color, opacity);
+			}
+		},
+		
+		showPoint: function(pt, color, opacity) {
+			color = color || 0xff0000;
+			opacity = opacity || 1;
+			let dotGeometry = new THREE.Geometry();
+			dotGeometry.vertices.push(new THREE.Vector3(pt.x, pt.y, pt.z));
+			let dotMaterial = new THREE.PointsMaterial({ 
+				size: 10,
+				sizeAttenuation: false,
+				color: color,
+				opacity: opacity,
+				transparent: true
+			});
+			let dot = new THREE.Points(dotGeometry, dotMaterial);
+			scene.add(dot);
+		},
+		
+		showVector: function(vector, origin, color) {
+			
+			color = color || 0xff0000;
+			let arrowHelper = new THREE.ArrowHelper(vector, origin, vector.length(), color);
+			scene.add(arrowHelper);
+		},
+		
+		drawLine: function(pt1, pt2) {
+			
+			let material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+			let geometry = new THREE.Geometry();
+			geometry.vertices.push(new THREE.Vector3(pt1.x, pt1.y, pt1.z));
+			geometry.vertices.push(new THREE.Vector3(pt2.x, pt2.y, pt2.z));
+			
+			let line = new THREE.Line(geometry, material);
+			scene.add(line);
+		},
+		
+		getDistance: function(pt1, pt2) { // create point class?
+			
+			let squirt = Math.pow((pt2.x - pt1.x), 2) + Math.pow((pt2.y - pt1.y), 2) + Math.pow((pt2.z - pt1.z), 2);
+			return Math.sqrt(squirt);
+		},
+		
+		createVector: function(pt1, pt2) {
+			return new THREE.Vector3(pt2.x - pt1.x, pt2.y - pt2.y, pt2.z - pt1.z);
+		},
+		
+		getMidpoint: function(pt1, pt2) {
+			
+			let midpoint = {};
+			
+			midpoint.x = (pt1.x + pt2.x) / 2;
+			midpoint.y = (pt1.y + pt2.y) / 2;
+			midpoint.z = (pt1.z + pt2.z) / 2;
+			
+			return midpoint;
+		},
+		
+		createTriangle: function(pt1, pt2, pt3) { // return geometry
+			let triangleGeometry = new THREE.Geometry();
+			triangleGeometry.vertices.push(new THREE.Vector3(pt1.x, pt1.y, pt1.z));
+			triangleGeometry.vertices.push(new THREE.Vector3(pt2.x, pt2.y, pt2.z));
+			triangleGeometry.vertices.push(new THREE.Vector3(pt3.x, pt3.y, pt3.z));
+			triangleGeometry.faces.push(new THREE.Face3(0, 1, 2));
+			triangleGeometry.computeFaceNormals();
+			return triangleGeometry;
+		},
+		
+		activateAxesHelper: function() {
+			
+			let self = this;
+			let axesHelper = new THREE.AxesHelper(self.settings.axesHelper.axisLength);
+			scene.add(axesHelper);
+		},
+		
+		labelAxes: function() {
+			
+			let self = this;
+			if (self.settings.font.enable) {
+				let textGeometry = new THREE.TextGeometry('Y', self.settings.font.fontStyle);
+				let textMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+				let mesh = new THREE.Mesh(textGeometry, textMaterial);
+				textGeometry.translate(0, self.settings.axesHelper.axisLength, 0);
+				scene.add(mesh);
+				
+				textGeometry = new THREE.TextGeometry('X', self.settings.font.fontStyle);
+				textMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+				mesh = new THREE.Mesh(textGeometry, textMaterial);
+				textGeometry.translate(self.settings.axesHelper.axisLength, 0, 0);
+				scene.add(mesh);
+				
+				textGeometry = new THREE.TextGeometry('Z', self.settings.font.fontStyle);
+				textMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+				mesh = new THREE.Mesh(textGeometry, textMaterial);
+				textGeometry.translate(0, 0, self.settings.axesHelper.axisLength);
+				scene.add(mesh);
+			}
+		},
+		
+		loadFont: function() {
+			
+			let self = this;
+			let loader = new THREE.FontLoader();
+			let fontPath = '';
+			fontPath = 'assets/vendors/js/three.js/examples/fonts/helvetiker_regular.typeface.json';
+
+			loader.load(fontPath, function(font) { // success event
+				
+				console.log('Fonts loaded successfully.');
+				self.settings.font.fontStyle.font = font;
+				
+				self.begin();
+				if (self.settings.axesHelper.activateAxesHelper) self.labelAxes();
+			}, function(event) { // in progress event.
+				console.log('Attempting to load fonts.')
+			}, function(event) { // error event
+				
+				console.log('Error loading fonts. Webserver required due to CORS policy.');
+				self.settings.font.enable = false;
+				self.begin();
+			});
+		},
+		
+		/* 	Inputs: pt - point in space to label, in the form of object with x, y, and z properties; label - text content for label; color - optional */
+		labelPoint: function(pt, label, color) {
+			
+			let self = this;
+			if (self.settings.font.enable) {
+				color = color || 0xff0000;
+				let textGeometry = new THREE.TextGeometry(label, self.settings.font.fontStyle);
+				let textMaterial = new THREE.MeshBasicMaterial({ color: color });
+				let mesh = new THREE.Mesh(textGeometry, textMaterial);
+				textGeometry.translate(pt.x, pt.y, pt.z);
+				scene.add(mesh);
+			}
+		},
+		
+		setUpButtons: function() {
+			
+			let self = this;
+			let message = document.getElementById('message');
+			
+			document.addEventListener('keyup', function(event) {
+				
+				let L = 76;
+				
+				if (event.keyCode === L) {
+					
+					// do stuff when pressing key
+				}
+			});
+		},
+		
+		resizeRendererOnWindowResize: function() {
+
+			window.addEventListener('resize', utils.debounce(function() {
+				
+				if (renderer) {
+	
+					camera.aspect = window.innerWidth / window.innerHeight;
+					camera.updateProjectionMatrix();
+					renderer.setSize(window.innerWidth, window.innerHeight);
+				}
+			}, 250));
+		}
+	}
+}
